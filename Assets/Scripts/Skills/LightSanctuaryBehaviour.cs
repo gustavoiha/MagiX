e7d0029933e5974b32f0176fb4d25b92a3130ec1
Gustavo Iha <gustavoiha@gmail.com>
2016-04-17 00:13:22 -0300

    Terrain update

--- Assets/Scripts/Skills/LightSanctuaryBehaviour.cs
+++ Assets/Scripts/Skills/LightSanctuaryBehaviour.cs
@@ -3,25 +3,39 @@ using System.Collections;
 
 public class LightSanctuaryBehaviour : MonoBehaviour {
 
-    public Transform player; //Localização do player
+	//Localização do player
+    public Transform player;
 
-    public float range; //Distância do círculo
+	//Distância do círculo
+    public float range;
 
-    public static bool toogleSanctuary; //Ativar ou desativar santuário
+	//Ativar ou desativar santuário
+    public static bool toogleSanctuary;
 
 	// Use this for initialization
 	void Start () {
-        toogleSanctuary = false;//Santuário começa desativado
-        range = GetComponent<ParticleSystem>().transform.localScale.x; // Alcance terá mesma distância do Círculo de partículas
+		
+		//Santuário começa desativado
+        toogleSanctuary = false;
+
+		// Alcance terá mesma distância do Círculo de partículas
+        range = GetComponent<ParticleSystem>().transform.localScale.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = player.position; //O santuário se manterá no local do jogador(?)
-        if (toogleSanctuary)//Ao acionar o santuário
-        {
-            GetComponent<ParticleSystem>().Play();//Aciona as partículas
-            foreach (Collider col in Physics.OverlapSphere(player.position, range))//Todos no alcance do santuário sofrerão algum efeito
+
+		//O santuário se manterá no local do jogador(?)
+        transform.position = player.position;
+
+		//Ao acionar o santuário
+        if (toogleSanctuary) {
+
+			//Aciona as partículas
+            GetComponent<ParticleSystem>().Play();
+
+			//Todos no alcance do santuário sofrerão algum efeito
+            foreach (Collider col in Physics.OverlapSphere(player.position, range))
             {
                 if (col.tag == "Enemy")//Inimigos levarão dano
                 {
