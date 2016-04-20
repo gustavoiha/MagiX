76c962fb3731e32891700a23bf368f81d29a2e4d
Gustavo Iha <gustavoiha@gmail.com>
2016-04-20 17:56:11 -0300

    Update terrain

--- Assets/Scripts/Skills/LightRayBehaviour.cs
+++ Assets/Scripts/Skills/LightRayBehaviour.cs
@@ -4,20 +4,41 @@ using System.Collections;
 public class LightRayBehaviour : MonoBehaviour {
 
     public float growth = 1.1f; //Constante em que o raio crescerá
+
     public GameObject player; //Localização do Player
-    Vector3 scale; //Vetor para a escala atual do raio
+
+    private Vector3 scale; //Vetor para a escala atual do raio
+
+	private Vector3 originalPosition;
+
+	private float originalLength;
 
 	// Use this for initialization
 	void Start () {
         Destroy(gameObject, 1.0f); //Auto-destruição
         scale = transform.localScale; //Escala atual do raio
+
+		originalPosition = gameObject.transform.position;
+		originalLength   = gameObject.GetComponent<Renderer> ().bounds.size.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float yScale = transform.localScale.y; //Cria um float para segurar a posição Y que fará o raio crescer
+
+		//Cria um float para segurar a posição Y que fará o raio crescer
+        float yScale = transform.localScale.y;
+
         yScale *= growth; //Aumenta esse float de acordo com a constante
+
+		//Increases the local dimension y of the game object and moves it forward to avoid duplicate objects in same space 
+		//gameObject.transform.position += transform.forward * gameObject.GetComponent<Renderer>().bounds.size.y * (growth - 1.0f)/2.0f;
+
+		float deltaLength = gameObject.GetComponent<Renderer> ().bounds.size.z - originalLength;
+
+		gameObject.transform.position = originalPosition - deltaLength * transform.forward / 2.0f;
+
         transform.localScale = new Vector3(0,1,0) * yScale + scale; //Nova escala será a parte y somado com a parte antiga
+
 	}
 
 }
