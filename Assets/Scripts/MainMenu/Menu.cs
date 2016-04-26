a5791f22bdbd03248c889bb94df6bd70588f5a8d
Gustavo Iha <gustavoiha@gmail.com>
2016-04-26 10:29:39 -0300

    Added menu

---
+++ Assets/Scripts/MainMenu/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public void NewGameButton ()
+	{
+		SceneManager.LoadScene ("RebelVillage");
+	}
+	public void ExitGame ()
+	{
+		Application.Quit();
+	}
+}
