80f33affdb43c130719326a4a07583659ee03803
Igor <Igor>
2016-04-28 20:56:16 -0300

    ogros posicionados em alguns acampamentos

---
+++ Assets/Scripts/OgrePositionController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Ogros irão surgir no lugar das capsulas quando o player
+// estiver a uma distância mínima das capsulas
+public class OgrePositionController : MonoBehaviour {
+
+	// Ogro é o objeto a ser instanciado
+	public GameObject ogre; 
+
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.CompareTag("Player")) {
+			Instantiate (ogre, gameObject.transform.position, gameObject.transform.rotation);
+		}
+	}
+}
