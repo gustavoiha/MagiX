4931caec8bf285de4807ac0e86e1310eec2e95a7
Gustavo Ferreira Fogolin <Gustavo Ferreira Fogolin>
2016-04-30 16:34:02 -0300

    BossDome add

---
+++ Assets/Scripts/BossDomeBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossDomeBehaviour : MonoBehaviour {
+
+    public int y = 90;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, y, 0) * Time.deltaTime);
+	}
+}
