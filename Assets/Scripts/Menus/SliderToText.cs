8bf14bf89a7c51707ca3ed15b0e4daa6285fe520
Gustavo Iha <gustavoiha@gmail.com>
2016-05-26 15:32:21 -0300

    Slider to text script

---
+++ Assets/Scripts/Menus/SliderToText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class SliderToText : MonoBehaviour {
+
+	public string text;
+
+	private string textToShow;
+
+	public string numberPlacer;
+
+	public float startingValue;
+
+	private Text myText;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text> ();
+		AdjustValue (startingValue);
+	}
+
+	public void AdjustValue (float newValue){
+		textToShow = text.Replace (numberPlacer, newValue.ToString ("F2"));
+
+		myText.text = textToShow;
+	}
+
+}
