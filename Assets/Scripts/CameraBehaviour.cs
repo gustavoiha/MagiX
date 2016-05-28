84997d564ed8098a309fb3f702683008ad075a72
Gustavo Iha <gustavoiha@gmail.com>
2016-05-28 18:14:48 -0300

    Camera remake. Now with scroll zoom

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -9,30 +9,36 @@ using System.Collections;
 
 public class CameraBehaviour : MonoBehaviour {
 
-	public float CameraAngle = 35.0f;
+	//public float CameraAngle = 35.0f;
 
 	// Camera rotation limits. Upper limit should be positive, and Lower limit should be negative!
-	public float CameraAngleLimitUpper = 85.0f;
-	public float CameraAngleLimitLower = -10.0f;
+	//public float CameraAngleLimitUpper = 85.0f;
+	//public float CameraAngleLimitLower = -10.0f;
 
 	// Camera's distance from Pivot
-	public float CameraDistance = 9.5f;
+	public float startingDistance = 9.5f;
+
+	public float minDistance = 4.0f;
+
+	public float maxDistance = 20.0f;
 
 	// Camera's rotation speed in the X axis
 	//public float turnSpeedX = 80.0f;
 	//public float turnSpeedY = 80.0f;
 
-	public float minTurnSpeed = 20.0f;
-	public float maxTurnSpeed = 100.0f;
+	//public float minTurnSpeed = 20.0f;
+	//public float maxTurnSpeed = 100.0f;
 
-	private float turnSpeed;
+	//private float turnSpeed;
 
 	public bool isStatic = false;
+
+	public float scrollSpeed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
 
-		turnSpeed = (minTurnSpeed + maxTurnSpeed) / 2.0f;
+		//turnSpeed = (minTurnSpeed + maxTurnSpeed) / 2.0f;
 
 		float[] distances = gameObject.GetComponent<Camera>().layerCullDistances;
 
@@ -44,19 +50,28 @@ public class CameraBehaviour : MonoBehaviour {
 
 		gameObject.GetComponent<Camera>().layerCullDistances = distances;
 
-		if (!isStatic) {
-			transform.localPosition = new Vector3 (CameraDistance, 0, 0);
+		if (!isStatic) 
+			transform.localPosition = new Vector3 (0, 0, -startingDistance);
 
-			transform.rotation = Quaternion.LookRotation (-transform.localPosition);
-		}
+		//transform.rotation = Quaternion.LookRotation (-transform.localPosition);
 	}
 
 	void Update(){
 
-		if (!isStatic)
-			UpdateCameraCoordinatesRegular ();
+		if (isStatic)
+			return;
+
+		Vector3 position = transform.localPosition;
+
+		position.z += Input.GetAxis ("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime;
+
+		position.z = - Mathf.Max (minDistance, Mathf.Min (maxDistance, - position.z));
+
+		transform.localPosition = position;
+
 	}
 
+/*
 	public void UpdateCameraCoordinatesRegular (){
 
 		transform.rotation = Quaternion.LookRotation (- transform.localPosition);
@@ -73,42 +88,10 @@ public class CameraBehaviour : MonoBehaviour {
 
 		transform.RotateAround (transform.parent.transform.position, - transform.right, - turnSpeed * Input.GetAxis ("VerticalRotation") * Time.deltaTime);
 
-	}
-
-	public void AdjustTurnSpeed (float newSpeedPercent){
-		turnSpeed = minTurnSpeed + newSpeedPercent * (maxTurnSpeed - minTurnSpeed);
-	}
-
-	/*private void UpdateCameraCoordinatesRotate (Vector3 playerPosition, Vector3 playerRotation){
-
-		float turnX = Input.GetAxis ("VerticalRotation");
-
-		gameObject.transform.Rotate (turnX * turnSpeedX * Time.deltaTime, 0, 0);
-
-		// Angle to limit the camera's rotation
-		float newCamAngleX;
-
-		if (gameObject.transform.localRotation.eulerAngles.x <= 180)
-			newCamAngleX = Mathf.Min (transform.localRotation.eulerAngles.x, CameraAngleLimitUpper);
-		else
-			newCamAngleX = Mathf.Max (transform.localRotation.eulerAngles.x, 360 + CameraAngleLimitLower);
-
-		float moveX = Input.GetAxis ("HorizontalTranslation");
-		float moveZ = Input.GetAxis ("VerticalTranslation");
-
-		float extraAngleY = Mathf.Atan2 (moveX, moveZ) * Mathf.Rad2Deg;
-
-		Quaternion quaternion  = new Quaternion ();
-		quaternion.eulerAngles = new Vector3 (newCamAngleX, extraAngleY, 0);
-		transform.localRotation   = quaternion;
-
-		float camPosX = - CameraDistance * Mathf.Sin (extraAngleY * Mathf.Deg2Rad);
-		float camPosY = CameraPivot + CameraDistance * Mathf.Sin (transform.localRotation.eulerAngles.x * Mathf.Deg2Rad);
-		float camPosZ = - CameraDistance * Mathf.Cos (transform.localRotation.eulerAngles.x * Mathf.Deg2Rad) * Mathf.Cos (extraAngleY * Mathf.Deg2Rad);
-
-		//Debug.Log (camera.localRotation.eulerAngles);
-
-		transform.localPosition = new Vector3 (0, camPosY, camPosZ);
 	}*/
+
+	//public void AdjustTurnSpeed (float newSpeedPercent){
+	//	turnSpeed = minTurnSpeed + newSpeedPercent * (maxTurnSpeed - minTurnSpeed);
+	//}
 
 }
--- Assets/Scripts/CameraHolderBehaviour.cs
+++ Assets/Scripts/CameraHolderBehaviour.cs
@@ -8,13 +8,41 @@ public class CameraHolderBehaviour : MonoBehaviour {
 	// Height of the center of the camera's rotation (for example, the player body's center
 	public float CameraPivot = 2.5f;
 
+	// Camera rotation limits. Upper limit should be positive, and Lower limit should be negative!
+	public float CameraAngleLimitUpper = 85.0f;
+	public float CameraAngleLimitLower = -10.0f;
+
+	public float minTurnSpeed = 20.0f;
+	public float maxTurnSpeed = 100.0f;
+
+	private float turnSpeed;
+
 	// Use this for initialization
 	void Start () {
+
+		turnSpeed = (minTurnSpeed + maxTurnSpeed) / 2.0f;
+
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		transform.position = player.position + player.up * CameraPivot;
+		transform.rotation = player.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.position = player.position + player.up * CameraPivot;
+		
+		transform.position = player.position + player.up * CameraPivot;
+
+		transform.Rotate (Vector3.up * turnSpeed * Input.GetAxis ("HorizontalRotation") * Time.deltaTime, Space.World);
+
+		if ((Input.GetAxis ("VerticalRotation") > 0 && transform.rotation.eulerAngles.x >= CameraAngleLimitUpper && transform.rotation.eulerAngles.x < 180.0f) ||
+			(Input.GetAxis ("VerticalRotation") < 0 && transform.rotation.eulerAngles.x <= 360.0f + CameraAngleLimitLower && transform.rotation.eulerAngles.x >= 180.0f))
+			return;
+
+		transform.Rotate (Vector3.right * turnSpeed * Input.GetAxis ("VerticalRotation") * Time.deltaTime);
+
+	}
+
+	public void AdjustTurnSpeed (float newSpeedPercent){
+		turnSpeed = minTurnSpeed + newSpeedPercent * (maxTurnSpeed - minTurnSpeed);
 	}
 }
